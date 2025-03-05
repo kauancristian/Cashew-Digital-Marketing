0570ad2fdcb864b7713db8a583729329d3eddeb2
Kauan Cristian <kauancristian.lz08@gmail.com>
2025-03-04 23:56:24 -0300

    Add files via upload

---
+++ app/main/scroll.js
@@ -0,0 +1,265 @@
+window.sr = ScrollReveal({reset: false});
+
+sr.reveal('#imgs', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 850
+})
+
+sr.reveal('#p-sec2', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 200
+})
+
+
+sr.reveal('#opt1', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 600
+})
+
+sr.reveal('#opt2', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 800
+})
+
+sr.reveal('#opt3', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 1000
+})
+
+sr.reveal('#opt4', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 1100
+})
+
+sr.reveal('#p-sec3', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 200
+})
+
+sr.reveal('#formu', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 500
+})
+
+sr.reveal('#p-vantagens', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 200
+})
+
+sr.reveal('#vantagens', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 600
+})
+
+sr.reveal('#p-sucesso', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 200
+})
+
+sr.reveal('#img-pessoa', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 600
+})
+
+sr.reveal('#inf-sec-sucesso', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 900
+})
+
+sr.reveal('#spans', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 1000
+})
+
+sr.reveal('#p-funciona', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 200
+})
+
+sr.reveal('#fc1', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 400
+})
+
+sr.reveal('#fc2', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 600
+})
+
+sr.reveal('#fc3', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 800
+})
+
+sr.reveal('#p-clientes', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 200
+})
+
+sr.reveal('#cli1', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 400
+})
+
+sr.reveal('#cli2', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 600
+})
+
+sr.reveal('#cli3', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 800
+})
+
+sr.reveal('#p-planos', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 400
+})
+
+
+sr.reveal('#plano1', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 600
+})
+
+sr.reveal('#plano2', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 800
+})
+
+
+sr.reveal('#plano3', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 1000
+})
+
+
+sr.reveal('#plano4', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 1200
+})
+
+sr.reveal('#p-escolher', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 400
+})
+
+sr.reveal('#img-hacker', {
+    duration: 1600,
+    origin: 'left',
+    distance: '70px',
+    delay: 600
+})
+
+sr.reveal('#p-inf-escolher', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 600
+})
+
+sr.reveal('#icons-escolher', {
+    duration: 1600,
+    origin: 'right',
+    distance: '70px',
+    delay: 800
+})
+
+sr.reveal('#p-perca-tempo', {
+    duration: 1600,
+    origin: 'left',
+    distance: '70px',
+    delay: 400
+})
+
+sr.reveal('#p-apartir', {
+    duration: 1600,
+    origin: 'top',
+    distance: '70px',
+    delay: 600
+})
+
+sr.reveal('#criar-btn', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 800
+})
+
+sr.reveal('#foot', {
+    duration: 1600,
+    origin: 'bottom',
+    distance: '70px',
+    delay: 400
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
